5b940202ab193d07fed909d6b26405a4e973b8da
Aryeong <aryeong22@hanmail.net>
2022-12-01 21:38:33 +0900

    get

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000, function() {
-    
+app.listen(8080, function() {
+    console.log('listening on 8080');
+});
+
+// app.get('경로', function(요청, 응답) {});
+app.get('/pet', function(req, res) {
+    res.send('펫 용품 쇼핑 페이지입니다.');
 });
