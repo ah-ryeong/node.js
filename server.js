e87b205c14fb6ca1128dce7ff77ca7ce315b30eb
Aryeong <aryeong22@hanmail.net>
2022-12-08 21:41:02 +0900

    numbering posts

--- server.js
+++ server.js
@@ -40,11 +40,21 @@ app.get('/write', (req, res)  => {
 });
 
 app.post('/add', (req, res) => {
-    
-    db.collection('post').insertOne({title : req.body.title, date : req.body.date}, (error, result) => {
-        console.log('저장완료');
+    db.collection('counter').findOne({ name: '게시물갯수' }, (error, result) => {
+        console.log(result.totalPost);
+        const totalPost = result.totalPost;
+
+        db.collection('post').insertOne({_id : totalPost + 1, title : req.body.title, date : req.body.date}, (error, result) => {
+            console.log('저장완료');
+            db.collection('counter').updateOne(
+                { name: '게시물갯수' },
+                { $inc : { totalpost : 1 }}, 
+                (error, result) => {
+                    if(error) return console.log(`/add counter update error ::: ${error}`);
+                }
+            );
+        });
     });
-    
     res.send('전송완료');
 });
 
